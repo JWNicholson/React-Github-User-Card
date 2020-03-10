4f9a00ff135696078c7cf4a1ee3cdbc848730300
JW Nicholson <john.nicholson812@gmail.com>
2020-03-10 15:43:55 -0400

    basic layout complete. ready to start coding

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -3,7 +3,16 @@ import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
- 
+ state = {
+    user: {},
+    followers: []
+ };
+
+ //call to get my profile info
+
+ //call to get my followers
+
+ //render card with data and import MyFollowers and MyInfo
 
 }//end class App
 
